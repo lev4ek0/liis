b066572442a057fe889ec37b795c15f501e9bff4
Lev Belous <leva22.08.01@inbox.ru>
2022-02-09 18:14:52 +0300

    static files

--- liis/urls.py
+++ liis/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/', include('articles.urls')),
     path('api/auth/', include('authentication.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
