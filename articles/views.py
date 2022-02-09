8f04b273f161871c790f39ee63e201c345de93ba
Lev Belous <leva22.08.01@inbox.ru>
2022-02-09 23:08:58 +0300

    beautify code

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from authentication.models import User
 from .models import Article
 from .serializers import ArticleSerializer
@@ -11,7 +13,7 @@ class ArticleAPIView(APIView):
     serializer_class = ArticleSerializer
 
     def get(self, request, pk=None):
-        if request.user.is_authenticated and (request.user.role == User.AUTHOR or request.user.role == User.SUBSCRIBER):
+        if request.user.is_authenticated and request.user.role in [User.AUTHOR, User.SUBSCRIBER]:
             a = Article.objects.all()
         else:
             a = Article.objects.filter(type=Article.PUBLIC)
@@ -28,11 +30,11 @@ class ArticleAPIView(APIView):
                 serializer = self.serializer_class(data=data)
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
-                response = Response({"id": serializer.data.get('id', None)}, status=status.HTTP_200_OK)
+                response = Response({"id": serializer.data.get('id', None)}, status=status.HTTP_201_CREATED)
             else:
-                response = Response({'error': 'Only author can create article.'}, status=status.HTTP_400_BAD_REQUEST)
+                response = Response({'error': 'Only author can create article.'}, status=status.HTTP_403_FORBIDDEN)
         else:
-            response = Response({'error': 'Log in to create article.'}, status=status.HTTP_400_BAD_REQUEST)
+            response = Response({'error': 'Log in to create article.'}, status=status.HTTP_401_UNAUTHORIZED)
         return response
 
     def put(self, request, pk):
@@ -43,25 +45,24 @@ class ArticleAPIView(APIView):
                 serializer = self.serializer_class(instance=instance, data=data, partial=True)
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
-                response = Response({"id": pk}, status=status.HTTP_200_OK)
+                response = Response(status=status.HTTP_201_CREATED)
             else:
-                response = Response({'error': 'Only author can create article.'}, status=status.HTTP_400_BAD_REQUEST)
+                response = Response({'error': 'Only author can create article.'}, status=status.HTTP_403_FORBIDDEN)
         else:
-            response = Response({'error': 'Log in to create article.'}, status=status.HTTP_400_BAD_REQUEST)
+            response = Response({'error': 'Log in to create article.'}, status=status.HTTP_401_UNAUTHORIZED)
         return response
 
     def delete(self, request, pk):
         if request.user.is_authenticated:
             if request.user.role == User.AUTHOR:
                 try:
-                    instance = Article.objects.get(pk=pk)
+                    Article.objects.get(pk=pk).delete()
                 except ObjectDoesNotExist:
                     return Response({'error': f'Article with id={pk} doesn\'t exist'},
-                                    status=status.HTTP_400_BAD_REQUEST)
-                instance.delete()
-                response = Response({"id": pk}, status=status.HTTP_200_OK)
+                                    status=status.HTTP_404_NOT_FOUND)
+                response = Response(status=status.HTTP_204_NO_CONTENT)
             else:
-                response = Response({'error': 'Only author can create article.'}, status=status.HTTP_400_BAD_REQUEST)
+                response = Response({'error': 'Only author can create article.'}, status=status.HTTP_403_FORBIDDEN)
         else:
-            response = Response({'error': 'Log in to create article.'}, status=status.HTTP_400_BAD_REQUEST)
+            response = Response({'error': 'Log in to create article.'}, status=status.HTTP_401_UNAUTHORIZED)
         return response
