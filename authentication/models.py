746a39ecec316ae64a6713a0f22c954c40ef7d34
Lev Belous <leva22.08.01@inbox.ru>
2022-02-09 12:12:48 +0300

    user and usermanager

---
+++ authentication/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password):
+        if password is None:
+            raise TypeError('Users must have a password.')
+        if email is None:
+            raise TypeError('Users must have an email.')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    AUTHOR = 'Aut'
+    SUBSCRIBER = 'Sub'
+    RoleChoice = (
+        (AUTHOR, 'Author'),
+        (SUBSCRIBER, 'Subscriber'),
+    )
+
+    username = models.CharField(db_index=True, max_length=255, unique=True, null=True)
+    email = models.EmailField(db_index=True, unique=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=3, choices=RoleChoice, default=SUBSCRIBER)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
