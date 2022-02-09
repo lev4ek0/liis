40291ef3adbd5e8af0771a718dc1558f915421e2
Lev Belous <leva22.08.01@inbox.ru>
2022-02-09 19:13:16 +0300

    article to articles

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ArticleAPIView
 
 urlpatterns = [
-    path('articles/', ArticleAPIView.as_view()),
-    path('articles/<int:pk>/', ArticleAPIView.as_view())
+    path('article/', ArticleAPIView.as_view()),
+    path('article/<int:pk>/', ArticleAPIView.as_view())
 ]
