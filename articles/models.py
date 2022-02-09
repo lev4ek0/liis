39ccf08ffad0c4584ce74b9eae4cfa98afd486b2
Lev Belous <leva22.08.01@inbox.ru>
2022-02-09 12:13:09 +0300

    enums refactor

--- articles/models.py
+++ articles/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 class Articles(models.Model):
-    class TypeOfArticle(models.TextChoices):
-        PUBLIC = 'PUB'
-        PRIVATE = 'PRI'
+    PUBLIC = 'PUB'
+    PRIVATE = 'PRI'
+    TypeChoices = (
+        (PUBLIC, 'Public'),
+        (PRIVATE, 'Private'),
+    )
 
     author = models.IntegerField()
     header = models.CharField(max_length=30)
     text = models.CharField(max_length=200)
     creation_date = models.DateTimeField()
-    type = models.CharField(max_length=3, choices=TypeOfArticle, default=TypeOfArticle.PRIVATE)
+    type = models.CharField(max_length=3, choices=TypeChoices, default=PRIVATE)
