b75ec9ab8cd9a886863749dbad17686ba384aa1a
Lev Belous <leva22.08.01@inbox.ru>
2022-02-10 11:32:12 +0300

    add validation

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import validate_email
+
 from rest_framework import serializers
 
 from .models import User
@@ -7,6 +10,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'password')
+
+    def validate(self, attrs):
+        password = attrs['password']
+        email = attrs['email']
+        validate_email(email)
+        validate_password(password)
+        return {'password': password, 'email': email}
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
